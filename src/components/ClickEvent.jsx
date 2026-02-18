b65beccd1e40e1af5d5c9e24d39eec4fcc29a6f0
we <we2323@gmail.com>
2026-02-18 17:11:26 +0700

    update website

--- src/components/ClickEvent.jsx
+++ src/components/ClickEvent.jsx
@@ -1,14 +1,12 @@
-import { useLinkClickHandler } from "react-router-dom";
-
 function clickEvent() {
 
-    const ClickHandler = (event) => {
+    const handleClick = (event) => {
         window.alert(event.target.innerHTML);
     }
 
     return (
         <div>
-            <button onClick={(e) => ClickHandler(e)}>Click Me</button>
+            <button onClick={(e) => handleClick(e)}>Click Me</button>
         </div>
     )
 }
